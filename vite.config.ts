77743dbae695674984d7a1916216bdfd10578ddf
Maks Antkowiak <20324150+m-antkowiak@users.noreply.github.com>
2024-03-06 22:00:20 +0100

    chore: :wastebasket: project cleanup
    Remove any unnecessary files from Vue3 template

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,15 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import eslint from '@nabla/vite-plugin-eslint';
+import stylelint from 'vite-plugin-stylelint';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue(), eslint({ eslintOptions: { cache: false } }), stylelint()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
-})
+});
